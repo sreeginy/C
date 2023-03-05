b58d94a73cdfcdedafe99fed1bfa1da0de41a46c
Sreeginy Kodeeswaran <sreeginy98@gmail.com>
2023-03-05 18:20:18 +0530

    C - intro

---
+++ dataType.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main() {
+    int a,b,c;
+    printf("\n Enter the two numbers");
+    scanf("%d%d, &a, &b");
+    c = a+b;
+    printf("\n Total : %d", C )
+}
---
+++ welcom.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+void main() {
+    printf(\n Welcome to C Program Language);
+}
