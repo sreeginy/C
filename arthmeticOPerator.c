fd2766cf180fb6523ca3417ee2cb18858a80c686
Sreeginy Kodeeswaran <sreeginy98@gmail.com>
2023-03-06 22:35:39 +0530

    arthmeticOperator

---
+++ arthmeticOPerator.c
@@ -0,0 +1,16 @@
+#include<studio.h>
+void main() {
+    int a,b,c;
+    printf("\n Enter the number");
+    scanf("%d%d",&a,&b);
+    c = a + b;
+    printf("\n Total : %d" c);
+    c = a - b;
+    printf("\n Subraction : %d" c);
+    c = a* b;
+    printf("\n multiplication : %d" c);
+    c = a / b;
+    printf("\n Division : %d" c);
+    c = a / b;
+    printf("\n Mode : %d" c);
+}
---
+++ logicalOperator.c
@@ -0,0 +1,7 @@
+#include <studio.h>
+void main() {
+    int a,b;
+    printf("\n Enter the two numbers");
+    scanf("%d%d", &, &b);
+    printf("\nResult : %d", ((a>100)&&(b<=200)));
+}
