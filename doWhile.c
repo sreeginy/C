7ba97d0f6d4495cb30fd4a462f830c09f948365a
Sreeginy Kodeeswaran <sreeginy98@gmail.com>
2023-03-06 22:43:21 +0530

    update

---
+++ doWhile.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+#include <conio.h>
+void main() {
+    int i = 1;
+    do {
+        printf("\n %d", i)
+        i++;
+    
+    }while (i <= 10);
+    printf("\n End");
+}
---
+++ ifelse.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+void main() {
+    int age;
+    printf("\n Enter your age");
+    scanf("\n %d",&age);
+    if( age > 18) {
+        printf("\n You are eligible for vote")
+    }else {
+        printf("\n You are not eligible for vote")
+    }
+}
