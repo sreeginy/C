41a8142a84c5347fdc11d365dc0e158ccb5594f5
Sreeginy Kodeeswaran <sreeginy98@gmail.com>
2023-03-07 22:34:43 +0530

    Create returnType.c

---
+++ returnType.c
@@ -0,0 +1,13 @@
+#include<studio.h>
+int science();
+void main() {
+    int s;
+    s = science();
+    printf("\n Total strength : %d ", s);
+}
+
+int science() {
+    int c;
+    c = 100;
+    return(c);
+}
